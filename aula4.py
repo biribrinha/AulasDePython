62a7eba7f2727ee23a525bdcbbd630c466639343
Julia <55153493+juliabergman@users.noreply.github.com>
2022-03-28 14:18:03 -0300

    Add files via upload

---
+++ aula4.py
@@ -0,0 +1,93 @@
+########################################
+#vai contar de 0 até 100#
+# for x in range(101):
+#     print( x )
+
+########################################
+
+    #saber quais são os números primos ( Números primos são aqueles divisíveis apenas por 1 e por eles mesmos) do 0 ao 100#
+
+# a = int(input('Insira um número: '))
+#
+# div=0
+# for x in range(1, a+1):
+#
+#     #se tiver resto, ele não é divisível#
+#     resto = a % x
+#     print(x, resto)
+#     if resto == 0:
+#         div = div + 1 #versão simplificada: div += 1#
+# if div == 2:
+#     print('numero {} é primo' .format(a))
+# else:
+#     print('numero {} não é primo' .format(a))
+
+
+########################################
+
+#imprimir só os numeros primos de 0 a 100#
+
+# a = int (input('coloque um valor: '))
+# for num in range(a+1):
+#     div=0
+#     for x in range(1, num+1):
+#
+#         resto = num % x
+#         #print(x, resto)
+#         if resto == 0:
+#             div = div + 1
+#     if div == 2:
+#         print(num)
+
+###########################################
+
+#
+# nota = int(input('colocar nota'))
+
+#vai ficar digitando eternamente até colocar uma nota que seja menor ou igual a 10#
+# while nota > 10:
+#     nota = int(input('colocar nota novamente'))
+
+
+###########################################
+
+    # forçar uma repetição até o numero ser 10#
+# a = 0
+# while a <= 10:
+#     print(a)
+#     a += 1
+
+
+##################################
+
+#vai ficar digitando eternamente até colocar o numero correto #
+
+# a = int(input('primeiro bimestre: '))
+# while a > 10:
+#     a = int(input('as notas são de 0 a 100. Tente novamente'))
+# b = int(input('segundo bimestre: '))
+# while b > 10:
+#     b = int(input('as notas são de 0 a 100. Tente novamente'))
+# c = int(input('terceiro bimestre: '))
+# while c > 10:
+#     c = int(input('as notas são de 0 a 100. Tente novamente'))
+# d = int(input('quarto bimestre: '))
+# while d > 10:
+#     d = int(input('as notas são de 0 a 100. Tente novamente'))
+#
+#
+# media = (a + b + c + d) / 4
+#
+# print('media: {}' .format(media))
+
+
+
+
+
+
+
+
+
+
+
+
